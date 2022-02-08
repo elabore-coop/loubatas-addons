f6d5fda23000f637c44c15ac16ac3e382770f7b1
Stéphan Sainléger <contact@stephansainleger.fr>
2022-02-08 09:33:58 +0100

    [UPD] Change readonly strategy on considered_for_compensation_days

--- hr_timesheet_compensation_days_requests/models/hr_timesheet.py
+++ hr_timesheet_compensation_days_requests/models/hr_timesheet.py
@@ -6,6 +6,4 @@ class AccountAnalyticLine(models.Model):
     _name = "account.analytic.line"
     _inherit = "account.analytic.line"
 
-    considered_for_compensation_days = fields.Boolean(
-        readonly=True
-    )
+    considered_for_compensation_days = fields.Boolean()
