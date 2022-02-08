2f48cf88242b6682914b5f3f73665ec7571bc1dc
Stéphan Sainléger <contact@stephansainleger.fr>
2022-02-08 09:57:36 +0100

    [UPD] Increment versio number and black formatting

--- hr_timesheet_compensation_days_requests/__manifest__.py
+++ hr_timesheet_compensation_days_requests/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    'name': "hr_timesheet_compensation_days_requests",
-    'summary': """ Request compensation days based on timesheet """,
-    'license': 'AGPL-3',
-    'description': """
+    "name": "hr_timesheet_compensation_days_requests",
+    "summary": """ Request compensation days based on timesheet """,
+    "license": "AGPL-3",
+    "description": """
 =======================================
 hr_timesheet_compensation_days_requests
 =======================================
@@ -58,34 +58,30 @@ This module is maintained by ELABORE.
 Elabore is a cooperative corporation whose mission is to support the collaborative development of Odoo features and ecosystem for social and solidarity-based economy Organizations.
 
 """,
-
-    'author': "Elabore",
-    'website': "https://elabore.coop",
-
+    "author": "Elabore",
+    "website": "https://elabore.coop",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Leaves',
-    'version': '12.0.1.0.0',
-
+    "category": "Leaves",
+    "version": "12.0.1.1.0",
     # any module necessary for this one to work correctly
-    'depends': ['base',
-                'hr',
-                'hr_timesheet',
-                'hr_holidays',
-            ],
-
+    "depends": [
+        "base",
+        "hr",
+        "hr_timesheet",
+        "hr_holidays",
+    ],
     # always loaded
-    'data': [
-        'wizard/timesheet_compensation_days_request_view.xml',
-        'views/hr_timesheet_views.xml'
+    "data": [
+        "wizard/timesheet_compensation_days_request_view.xml",
+        "views/hr_timesheet_views.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [],
-    
-    'installable': True,
+    "demo": [],
+    "installable": True,
     # Install this module automatically if all dependency have been previously
     # and independently installed.  Used for synergetic or glue modules.
-    'auto_install': False,
-    'application': False,
+    "auto_install": False,
+    "application": False,
 }
