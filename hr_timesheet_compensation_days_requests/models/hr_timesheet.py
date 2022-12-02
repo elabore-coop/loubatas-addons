5619c0fa764bcafd14d33a44d50a3018a4d31881
Stéphan Sainléger <contact@stephansainleger.fr>
2022-12-02 16:31:47 +0100

    [IMP] Prevent copy of considered_for_compensation_days field

--- hr_timesheet_compensation_days_requests/models/hr_timesheet.py
+++ hr_timesheet_compensation_days_requests/models/hr_timesheet.py
@@ -7,3 +7,11 @@ class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     considered_for_compensation_days = fields.Boolean()
+
+    @api.multi
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        default = dict(default or {})
+        default.update(
+            considered_for_compensation_days=False)
+        return super(AccountAnalyticLine, self).copy(default)
